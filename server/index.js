cf05a9a9293582f5781a839d03496c9cd0498032
miguelTorresGranada <migueltorresgranada@gmail.com>
2022-02-03 20:07:07 +0100

    Setting up the serverside

--- server/index.js
+++ server/index.js
@@ -2,4 +2,10 @@ import express from 'express';
 import bodyParse from 'body-parser';
 import mongoose from 'mongoose';
 import cors from 'cors';
+import bodyParser from 'body-parser';
 
+const app = express();
+
+app.use(bodyParser.json({ limit: "30mb", exteded: true}));
+app.use(bodyParser.urlencoded({ limit: "30mb", exteded: true}));
+app.use(cors());
