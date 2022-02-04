cba1462df5dd5a56776f5f87f8513e8b7d2832bb
miguelTorresGranada <migueltorresgranada@gmail.com>
2022-02-04 19:13:09 +0100

    Beggining to create the routing

--- server/index.js
+++ server/index.js
@@ -1,11 +1,23 @@
 import express from 'express';
-import bodyParse from 'body-parser';
+import bodyParser from 'body-parser';
 import mongoose from 'mongoose';
 import cors from 'cors';
-import bodyParser from 'body-parser';
+import postRoutes from './routes/posts.js';
+import CONNECTION_URL from './connectionURL.js';
+
 
 const app = express();
 
-app.use(bodyParser.json({ limit: "30mb", exteded: true}));
-app.use(bodyParser.urlencoded({ limit: "30mb", exteded: true}));
+app.use('/posts', postRoutes);
+
+app.use(bodyParser.json({ limit: "30mb", extended: true}));
+app.use(bodyParser.urlencoded({ limit: "30mb", extended: true}));
 app.use(cors());
+
+const PORT = process.env.PORT || 5000;
+
+mongoose.connect(CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
+    .then(() => app.listen(PORT, () => console.log(`Server running on port: ${PORT}`)))
+    .catch((e) => console.log(e.message) );
+
+
